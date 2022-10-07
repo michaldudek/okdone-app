30add6396b641666beeebe6850026394f28847ac
Michał Pałys-Dudek <michal@pnd.io>
2022-10-08 01:36:02 +0200

    ✨ Go for full screen design. (#2)

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,15 +1,7 @@
 import styled from '@emotion/styled';
-import { MediaQuery, pxToRem } from 'styles';
 
 export const AppContainer = styled.main`
   background-color: var(--bg-primary);
   width: 100%;
   height: 100%;
-
-  ${MediaQuery.Tablet} {
-    width: 700px;
-    height: 90vh;
-    /* border-radius: ${pxToRem(8)}; */
-    box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
-  }
 `;
