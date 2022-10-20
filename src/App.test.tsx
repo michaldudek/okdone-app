41f0a0c76315168b9f165257e56f9c135ef5aeb9
Michal Palys-Dudek <michal@pnd.io>
2022-10-20 19:18:06 +0200

    🔥 Remove out of date test.

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders "OK, Done!" app name', () => {
-  render(<App />);
-  const element = screen.getByText('OK, Done!');
-  expect(element).toBeInTheDocument();
-});
